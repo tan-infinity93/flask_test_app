085a2905a351bed79efd4dbd2e6f328a62b95a67
tan-infinity93 <tan-infinity@live.com>
2019-03-30 12:15:54 +0530

    test flask app

---
+++ app.py
@@ -0,0 +1,23 @@
+# Import Modules:
+
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+
+# Create App Instance:
+
+app = Flask(__name__)
+
+
+# API Routes:
+
+@app.route('/')
+def home():
+
+	return jsonify({'message': 'Welcome to API Home'}), 200, {'Content-Type': 'application/json'}
+
+
+# Run App:
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=5001, debug=True)
