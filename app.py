7725b64ec4d48b21af119d50fc4a4bd1f59abce0
tan-infinity93 <tan-infinity@live.com>
2019-03-30 12:58:45 +0530

    added app get method v1.1

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def home():
 
 	return jsonify({'message': 'Welcome to API Home'}), 200, {'Content-Type': 'application/json'}
 
-@app.route('/get)
+@app.route('/get')
 def home1():
 
 	return jsonify({'message': 'Welcome to API Get'}), 200, {'Content-Type': 'application/json'}
